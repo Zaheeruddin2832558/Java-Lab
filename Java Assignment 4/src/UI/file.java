e1c284e2ddb714e4392df9b2a71eaa51db1dca9d
Zaheeruddin2832558 <siddiqui.moh@northeastern.edu>
2023-10-11 23:30:28 -0400

    Assignment

---
+++ Java Assignment 4/src/UI/file.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UI;
+
+import java.io.File;
+
+/**
+ *
+ * @author zaheeruddin
+ */
+class file {
+
+    static File getSelectedFile() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
